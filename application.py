c50cdd945f5a8025fd472cafafc5eb53981221f9
FizzyPM <munjal.pranav14@gmail.com>
2018-04-23 22:57:41 +0530

    working server

--- application.py
+++ application.py
@@ -1,15 +1,11 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session,render_template,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-
-# Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -17,10 +13,35 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine('postgresql://postgres:fizzzyme@localhost:5432/project1')
 db = scoped_session(sessionmaker(bind=engine))
 
-
 @app.route("/")
-def index():
-    return "Project 1: TODO"
+def signup():
+    return render_template("signup.html")
+
+@app.route("/create",methods=["POST"])
+def create():
+    fname = request.form.get("firstname")
+    lname = request.form.get("lastname")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    db.execute("INSERT INTO users (fname,lname,username,password) VALUES (:fname, :lname ,:uname,:pass)",
+            {"fname": fname, "lname": lname,"uname":username,"pass":password})
+    db.commit()
+    return render_template("success.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/search",methods=["POST"])
+def search():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if db.execute("SELECT * FROM users WHERE username = :username", {"username": username}).rowcount == 0:
+        return render_template("error.html",message="No such user with that username.")
+    user = db.execute("SELECT * FROM users WHERE username = :username", {"username": username }).fetchone()
+    if(user.password != password):
+        return render_template("error.html",message="Wrong Password")
+    return render_template("success.html")
